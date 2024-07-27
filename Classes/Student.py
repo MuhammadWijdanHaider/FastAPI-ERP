f3ac10be484735dd03080a353987807a7d151399
Wijdan <ghostwijdan12@gmail.com>
2024-07-27 15:56:17 +0500

    This is the good stuff

---
+++ Classes/Student.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+
+class student:
+    def __init__(self):
+        self.name: str
+        self.father_name: str
+        self.registration_number: int
+        self.id: int
+        self.dob: datetime
+
